eaa137798b5f2ba69b911721ac2ceb56e2c5a9c2
gehn <gehn@zhaw.ch>
2022-06-08 17:43:02 +0200

    Adds .apk file ending for apks check

--- data_analysis/data_analyser.py
+++ data_analysis/data_analyser.py
@@ -19,7 +19,7 @@ class DataAnalyser():
         not_collected_apks = 0
         for apkleaks_apk in apkleaks_apks:
             if apkleaks_apk+'.apk' in firmwaredroid_apkleaks_data:
-                results = firmwaredroid_apkleaks_data[apkleaks_apk]
+                results = firmwaredroid_apkleaks_data[apkleaks_apk+'.apk']
                 stored_entries_counter += self.store_overlap_apks(results, apkleaks_apk)
             else:
                 not_collected_apks += 1
