b4e842a69f072b908a19d0f0db30f6926fb5dcc0
gehn <gehn@zhaw.ch>
2022-06-09 15:37:49 +0200

    Adds exclusion of JSON Web Token

--- data_analysis/firmwaredroid_analyser.py
+++ data_analysis/firmwaredroid_analyser.py
@@ -78,7 +78,7 @@ class FirmwareDroidAnalyser():
 
                 apkleaks_information_with_appnames["secret_size"] = 0
                 for apkleaks_result in apkleaks_information_with_appnames["results"]["results"]:
-                    if apkleaks_result["name"] != "LinkFinder":
+                    if apkleaks_result["name"] != "LinkFinder" and apkleaks_result["name"] != "JSON_Web_Token":
                         apkleaks_information_with_appnames["secret_size"] += len(apkleaks_result["matches"])
  
                 apkleaks_results_with_result_length.append(apkleaks_information_with_appnames)
